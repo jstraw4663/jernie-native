3e58076a6c530ff83b4f320820be06a8fdf8ee7c
jstraw4663 <jstraw4663@gmail.com>
2026-05-31 02:09:44 +0000

    test: add Firebase database mock and install @testing-library/react-native

---
+++ __mocks__/@react-native-firebase/database.ts
@@ -0,0 +1,13 @@
+// Jest manual mock for @react-native-firebase/database.
+// This module is a JSI native module and cannot run in Node.js.
+// Activated automatically when jest.mock('@react-native-firebase/database') is called.
+
+const mockSet = jest.fn().mockResolvedValue(undefined);
+const mockOff = jest.fn();
+const mockOnce = jest.fn();
+const mockOn = jest.fn();
+const mockRef = jest.fn(() => ({ once: mockOnce, on: mockOn, off: mockOff, set: mockSet }));
+const mockDatabase = jest.fn(() => ({ ref: mockRef }));
+
+export { mockRef, mockOnce, mockOn, mockOff, mockSet };
+export default mockDatabase;
